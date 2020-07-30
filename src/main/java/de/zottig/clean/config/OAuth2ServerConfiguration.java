fa1f6ad80aff79c848626a58d7dca6ca268e83c4
Ilja Winokurow <test@test.de>
2020-07-30 19:59:55 +0200

    Try

--- src/main/java/de/zottig/clean/config/OAuth2ServerConfiguration.java
+++ src/main/java/de/zottig/clean/config/OAuth2ServerConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -25,12 +26,13 @@ public class OAuth2ServerConfiguration {
 
 	private static final String RESOURCE_ID = "restservice";
 
+	
 	@Configuration
 	@EnableResourceServer
 	protected static class ResourceServerConfiguration
 			extends
 				ResourceServerConfigurerAdapter { 
-
+		
 		@Override
 		public void configure(ResourceServerSecurityConfigurer resources) {
 			resources.resourceId(RESOURCE_ID);
@@ -53,6 +55,9 @@ public class OAuth2ServerConfiguration {
 			extends
 				AuthorizationServerConfigurerAdapter {
 
+		@Autowired
+		private PasswordEncoder passwordEncoder;
+		
 		private TokenStore tokenStore = new InMemoryTokenStore();
 
 		@Autowired
@@ -75,10 +80,11 @@ public class OAuth2ServerConfiguration {
 		@Override
 		public void configure(ClientDetailsServiceConfigurer clients)
 				throws Exception {
+
 			clients.inMemory().withClient("clientapp")
 					.authorizedGrantTypes("password", "refresh_token")
 					.authorities("USER").scopes("read", "write", "tasks")
-					.resourceIds(RESOURCE_ID).secret("{bcrypt}$2y$12$bsvLFNcNrCVhM17ZluqUYuRSfzhUtzDQFHKHROUE.xfb2AVoexVwi");
+					.resourceIds(RESOURCE_ID).secret(passwordEncoder.encode("123456"));
 		}
 
 		@Bean
