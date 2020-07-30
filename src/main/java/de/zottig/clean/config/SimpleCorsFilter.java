97d4013bdc044bab21a6dcdaca03795daf0208cb
Ilja Winokurow <test@test.de>
2020-07-30 17:44:46 +0200

    Try to Fix

--- src/main/java/de/zottig/clean/config/SimpleCorsFilter.java
+++ src/main/java/de/zottig/clean/config/SimpleCorsFilter.java
@@ -11,12 +11,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
+//@Component
+//@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCorsFilter implements Filter {
 
 	public SimpleCorsFilter() {
