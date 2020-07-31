5d108d62026e4be55d7352a89ce1ef90a5f6688e
Ilja Winokurow <test@test.de>
2020-07-31 06:14:47 +0200

    Revert

--- src/main/java/de/zottig/clean/config/OAuth2ServerConfiguration.java
+++ src/main/java/de/zottig/clean/config/OAuth2ServerConfiguration.java
@@ -5,10 +5,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -19,10 +23,15 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.google.common.collect.ImmutableList;
 
 import de.zottig.clean.service.CustomUserDetailsService;
 @Configuration
-@Order(20)
 public class OAuth2ServerConfiguration {
 
 	private static final String RESOURCE_ID = "restservice";
@@ -80,7 +89,7 @@ public class OAuth2ServerConfiguration {
 			clients.inMemory().withClient("clientapp")
 					.authorizedGrantTypes("password", "refresh_token")
 					.authorities("USER").scopes("read", "write", "tasks")
-					.resourceIds(RESOURCE_ID).secret("123456");
+					.resourceIds(RESOURCE_ID).secret("{noop}123456");
 		}
 
 		@Bean
@@ -94,4 +103,47 @@ public class OAuth2ServerConfiguration {
 
 	}
 
+	@Configuration
+	@EnableWebSecurity
+	protected static class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		@Autowired
+		private CustomUserDetailsService userDetailsService;
+
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth)
+				throws Exception {
+			auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		}
+
+		@Override
+		@Bean
+		public AuthenticationManager authenticationManagerBean() throws Exception {
+			return super.authenticationManagerBean();
+		}
+
+		@Bean
+		public PasswordEncoder passwordEncoder() {
+			return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().csrfTokenRepository(
+					CookieCsrfTokenRepository.withHttpOnlyFalse());
+			// http.authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll();
+			http.cors();
+		}
+
+		@Bean
+		public CorsConfigurationSource corsConfigurationSource() {
+			final CorsConfiguration configuration = new CorsConfiguration();
+			configuration.setAllowedOrigins(ImmutableList.of("*"));
+			configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST",
+					"PUT", "DELETE", "PATCH", "OPTIONS"));
+			final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			source.registerCorsConfiguration("/**", configuration);
+			return source;
+		}
+	}
 }
--- src/main/java/de/zottig/clean/config/WebSecurityConfiguration.java
+++ src/main/java/de/zottig/clean/config/WebSecurityConfiguration.java
@@ -1,77 +1,53 @@
 package de.zottig.clean.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.google.common.collect.ImmutableList;
 
-import de.zottig.clean.service.CustomUserDetailsService;
-
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableWebSecurity
-@Order(10)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private CustomUserDetailsService userDetailsService;
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
-	}
-
-	@Override
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
-
-	@Bean
-	public BCryptPasswordEncoder encoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().csrfTokenRepository(
-				CookieCsrfTokenRepository.withHttpOnlyFalse());
-		// http.authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll();
-		http.cors();
-	}
-
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		final CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(ImmutableList.of("*"));
-		configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST",
-				"PUT", "DELETE", "PATCH", "OPTIONS"));
-		List<String> headers = new ArrayList<>();
-		headers.add("Origin");
-		headers.add("Content-Type");
-		headers.add("x-requested-with");
-		headers.add("authorization");
-		configuration.setAllowedHeaders(headers);
-		configuration.setExposedHeaders(headers);
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
+//	@Autowired
+//	private CustomUserDetailsService userDetailsService;
+//
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth)
+//			throws Exception {
+//		auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+//	}
+//
+//	@Override
+//	@Bean
+//	public AuthenticationManager authenticationManagerBean() throws Exception {
+//		return super.authenticationManagerBean();
+//	}
+//
+//	@Bean
+//	public BCryptPasswordEncoder encoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.csrf().csrfTokenRepository(
+//				CookieCsrfTokenRepository.withHttpOnlyFalse());
+//		// http.authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll();
+//		http.cors();
+//	}
+//
+//	@Bean
+//	public CorsConfigurationSource corsConfigurationSource() {
+//		final CorsConfiguration configuration = new CorsConfiguration();
+//		configuration.setAllowedOrigins(ImmutableList.of("*"));
+//		configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST",
+//				"PUT", "DELETE", "PATCH", "OPTIONS"));
+//		List<String> headers = new ArrayList<>();
+//		headers.add("Origin");
+//		headers.add("Content-Type");
+//		headers.add("x-requested-with");
+//		headers.add("authorization");
+//		configuration.setAllowedHeaders(headers);
+//		configuration.setExposedHeaders(headers);
+//		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", configuration);
+//		return source;
+//	}
 }
